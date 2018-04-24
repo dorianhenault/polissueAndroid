ed8a07bd7ed429708a5fe384c635b8306eee590d
Simon Serrano <simon.serrano@etu.unice.fr>
2018-04-24 16:26:44 +0200

    Added model

---
+++ app/src/main/java/ihm/si3/fr/unice/polytech/polissue/model/Emergency.java
@@ -0,0 +1,14 @@
+package ihm.si3.fr.unice.polytech.polissue.model;
+
+
+
+enum Emergency{
+
+    HIGH,
+    MEDIUM,
+    LOW;
+
+    Emergency() {
+    }
+
+}
--- app/src/main/java/ihm/si3/fr/unice/polytech/polissue/model/IssueModel.java
+++ app/src/main/java/ihm/si3/fr/unice/polytech/polissue/model/IssueModel.java
@@ -1,8 +1,107 @@
 package ihm.si3.fr.unice.polytech.polissue.model;
 
-public class IssueModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Date;
+
+/**
+ * Class that models an issue
+ */
+public class IssueModel implements Parcelable{
+
+    private int id;
+    private String title;
+    private String description;
+    private Date date;
+    private Emergency emergency;
+    private Location location;
+    private User user;
+    private String imageURL;
 
 
+    public IssueModel() {
+        // Default constructor required for calls to DataSnapshot.getValue(IssueModel.class)
+    }
+
+    /**
+     * Minimalistic constructor for an issue
+     * @param id the id of the issue
+     * @param title the title of the issue
+     * @param description the description of the issue
+     * @param date the date of declaration of the issue
+     * @param emergency the emergency level of the issue
+     * @param user the user who declared the issue
+     */
+    public IssueModel(int id, String title, String description, Date date, Emergency emergency, User user) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.emergency = emergency;
+        this.user = user;
+    }
+
+    /**
+     * Full constructor for an issue
+     * @param id the id of the issue
+     * @param title the title of the issue
+     * @param description the description of the issue
+     * @param date the date of declaration of the issue
+     * @param emergency the emergency level of the issue
+     * @param location the location of the issue
+     * @param user the user who declared the issue
+     * @param imageURL the image URL of the issue
+     */
+    public IssueModel(int id, String title, String description, Date date, Emergency emergency, Location location, User user, String imageURL) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.emergency = emergency;
+        this.location = location;
+        this.user = user;
+        this.imageURL = imageURL;
+    }
 
 
+    protected IssueModel(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        description = in.readString();
+        date = new Date(in.readLong());
+        emergency = Emergency.valueOf(in.readString());
+        location = in.readParcelable(Location.class.getClassLoader());
+        user = in.readParcelable(User.class.getClassLoader());
+        imageURL = in.readString();
+    }
+
+    public static final Creator<IssueModel> CREATOR = new Creator<IssueModel>() {
+        @Override
+        public IssueModel createFromParcel(Parcel in) {
+            return new IssueModel(in);
+        }
+
+        @Override
+        public IssueModel[] newArray(int size) {
+            return new IssueModel[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeLong(date.getTime());
+        dest.writeString(emergency.name());
+        dest.writeParcelable(location, PARCELABLE_WRITE_RETURN_VALUE);
+        dest.writeParcelable(user, PARCELABLE_WRITE_RETURN_VALUE);
+        dest.writeString(imageURL);
+    }
 }
