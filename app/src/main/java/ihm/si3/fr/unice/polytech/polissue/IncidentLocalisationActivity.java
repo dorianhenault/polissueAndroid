d213876730012911ea269b913fc8f9b31edd1f62
Dorian Henault <dorian.henault@etu.unice.fr>
2018-05-08 17:34:31 +0200

    On peut pour le moment aller voir sur Gmaps la position exacte d'un incident

--- app/src/main/java/ihm/si3/fr/unice/polytech/polissue/IncidentLocalisationActivity.java
+++ app/src/main/java/ihm/si3/fr/unice/polytech/polissue/IncidentLocalisationActivity.java
@@ -1,5 +1,6 @@
 package ihm.si3.fr.unice.polytech.polissue;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -7,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -60,6 +63,10 @@ public class IncidentLocalisationActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.incident_location_gmaps);
+        Button button=(Button) findViewById(R.id.validatePosition) ;
+        button.setOnClickListener(v -> {
+                startActivity(new Intent(this, MainPageActivity.class));
+        });
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
