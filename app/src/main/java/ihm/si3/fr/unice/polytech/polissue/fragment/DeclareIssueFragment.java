2ad5bac410b4056e15cd67d9b24b0eef3e86f789
Gregoire Peltier <pg501842@etu.unice.fr>
2018-05-11 18:37:54 +0200

    Added camera and file interaction for picture choosing

--- app/src/main/java/ihm/si3/fr/unice/polytech/polissue/fragment/DeclareIssueFragment.java
+++ app/src/main/java/ihm/si3/fr/unice/polytech/polissue/fragment/DeclareIssueFragment.java
@@ -1,7 +1,11 @@
 package ihm.si3.fr.unice.polytech.polissue.fragment;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
@@ -14,19 +18,24 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.Date;
+import java.util.Objects;
 
 import ihm.si3.fr.unice.polytech.polissue.DataBaseAccess;
 import ihm.si3.fr.unice.polytech.polissue.R;
 import ihm.si3.fr.unice.polytech.polissue.model.Emergency;
 import ihm.si3.fr.unice.polytech.polissue.model.IssueModel;
 
+import static android.app.Activity.RESULT_OK;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DeclareIssueFragment extends Fragment{
 
+    private static final int ADD_IMAGE = 1;
+    private static final int TAKE_PICTURE = 2;
     private static final String TAG = "DeclareIssueFragment";
-    private ImageButton validButton, addImage, currentLocation, cancelButton;
+    private ImageButton validButton, addImage, takePicture, currentLocation, cancelButton;
     private ImageView image;
     private EditText title, description, declarer, location;
     private SeekBar emergencyLevel;
@@ -51,6 +60,7 @@ public class DeclareIssueFragment extends Fragment{
         final View view = inflater.inflate(R.layout.fragment_declare_issue, container, false);
         validButton = view.findViewById(R.id.validButton);
         addImage = view.findViewById(R.id.addImageButton);
+        takePicture = view.findViewById(R.id.takePicutreButton);
         currentLocation = view.findViewById(R.id.currentLocationButton);
         cancelButton = view.findViewById(R.id.cancelButton);
         image = view.findViewById(R.id.issueImagePreview);
@@ -79,9 +89,16 @@ public class DeclareIssueFragment extends Fragment{
         }));
 
         addImage.setOnClickListener(v -> {
-            //TODO implement adding an image
+            final Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            galleryIntent.setType("image/*");
+            startActivityForResult(galleryIntent, ADD_IMAGE);
         });
-
+        takePicture.setOnClickListener(v -> {
+            final Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (cameraIntent.resolveActivity(Objects.requireNonNull(getContext()).getPackageManager()) != null) {
+                startActivityForResult(cameraIntent, TAKE_PICTURE);
+            }
+        });
         currentLocation.setOnClickListener(v -> {
             //TODO implement the use of the location from the GPS sensor
         });
@@ -108,5 +125,20 @@ public class DeclareIssueFragment extends Fragment{
             ok = false;
         }
         return ok;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ADD_IMAGE && resultCode == RESULT_OK) {
+            Uri result = data.getData();
+            if (result != null) {
+                image.setImageURI(result);
+            }
+        } else if (requestCode == TAKE_PICTURE) {
+            Bundle extras = data.getExtras();
+            Bitmap preview = (Bitmap) extras.get("data");
+            image.setImageBitmap(preview);
+        }
     }
 }
