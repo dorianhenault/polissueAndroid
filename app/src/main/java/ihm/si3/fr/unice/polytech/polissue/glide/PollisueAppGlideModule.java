03a744ac8bdf6f5befe66435dcf18ece5e83df32
Gregoire Peltier <pg501842@etu.unice.fr>
2018-05-18 15:41:53 +0200

    Switch to Glide library for image loading optimisation

---
+++ app/src/main/java/ihm/si3/fr/unice/polytech/polissue/glide/PollisueAppGlideModule.java
@@ -0,0 +1,21 @@
+package ihm.si3.fr.unice.polytech.polissue.glide;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.InputStream;
+
+@GlideModule
+public class PollisueAppGlideModule extends AppGlideModule {
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        registry.append(StorageReference.class, InputStream.class, new FirebaseImageLoader.Factory());
+    }
+}
